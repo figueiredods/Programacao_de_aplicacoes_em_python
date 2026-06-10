9979760dc9804c713f29106bff1b453ca2d8389c
Daniel Figueiredo <figueiredo.ds2018@gmail.com>
2026-06-10 22:58:29 +0100

    2026_06_10_22_59

---
+++ exercicios/worksheet_4.py
@@ -0,0 +1,11 @@
+# 1: Crie uma função que calcule a área de retângulo
+
+# 2: Calcular o salário semanal de um colaborador, que recebe à hora, a partir apenas do nº de horas que
+
+# este trabalhou. Sabe-se que o número de horas semanais é 36 e que o preço à hora é de 7,5€. Se o colaborador fizer horas extra (mais de 36 horas) recebe 10€ por cada hora extra. Note que não pode pedir ao utilizador para indicar as horas extra, pois é um dado que pode ser calculado
+
+# 3: uma função em Python que recebe dois números e um operador (+, -, *, /) e retorna o resultado da operação
+
+# 4: Crie um menu usando a instrução match ( 1- Query, 2-Register , 3- Delete , 4- sair) , use o default também.
+
+# 5: Faça uma função que imprima a tabuada
